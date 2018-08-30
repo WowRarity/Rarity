0e2cb5ff7f421ed3f8294e0b1105d0776ebd70fe
Sacred Duckwhale <duckwhale@mail.ru>
2018-08-30 23:07:25 +0200

    Fixed typo in debug output

--- Core.lua
+++ Core.lua
@@ -2557,7 +2557,7 @@ local islandExpeditionCollectibles = { -- List of collectibles (so we don't have
 
 function R:OnIslandCompleted(mapID, winner)
 
-	R:Debug("Detected completion for Island Expedition: " .. (islandMapIDs[mapID] or "Unknown Map") .. " (mapID = " .. tostring(mapIDs) .. .. ")")
+	R:Debug("Detected completion for Island Expedition: " .. (islandMapIDs[mapID] or "Unknown Map") .. " (mapID = " .. tostring(mapIDs) .. ")")
 	
 	if islandMapIDs[mapID] then -- Is a relevant map -> Add attempts for all collectibles (for now, I'm assuming they just drop from everything at the same rate. This may have to be revised once more data is available...)
 	
