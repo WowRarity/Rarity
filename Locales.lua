f83bec804a56c9c451a0f2f9784946ded7218f66
Sacred Duckwhale <duckwhale@mail.ru>
2019-07-10 14:54:54 +0200

    Options: Added new menu for experimental features

--- Locales.lua
+++ Locales.lua
@@ -1,6 +1,11 @@
 local L
 L = LibStub("AceLocale-3.0"):NewLocale("Rarity", "enUS", true)
 
+L["Experimental"] = true
+L["Verify item database on login"] = true
+L["Run the verification routine automatically when logging in. It can always be run manually by typing %s"] = true
+L["Advanced"] = true
+
 L["Verifying database..."] = true
 L["Verification complete! Everything appears to be in order..."] = true
 L["Verifying entry: %s ..."] = true
