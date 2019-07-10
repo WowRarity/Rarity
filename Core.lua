b4fe3deb042335e9336f337b1e1ed3b4c0a51b21
Sacred Duckwhale <duckwhale@mail.ru>
2019-07-10 14:57:28 +0200

    Refactoring: Moved verification routine to separate function

--- Core.lua
+++ Core.lua
@@ -903,6 +903,40 @@ function R:BarAnchorClicked(cbk, group, button)
  self.db.profile.bar.relativePoint = relativePoint
 end
 
+-- TODO: Move elsewhere (in the final refactoring pass)
+function R:VerifyItemDB()
+
+	local DBH = self.DatabaseMaintenanceHelper
+			local ItemDB = self.db.profile.groups.items
+			local PetDB = self.db.profile.groups.pets
+			local MountDB = self.db.profile.groups.mounts
+			local UserDB = self.db.profile.groups.user
+			local DB = { ItemDB, PetDB, MountDB }
+
+			self:Print(L["Verifying database..."])
+			
+			local numErrors = 0
+			
+			for category, entry in pairs(DB) do
+				for item, fields in pairs(entry) do
+					
+					if type(fields) == "table" then
+					
+						self:Debug(format(L["Verifying entry: %s ..."], item))
+						local isEntryValid = DBH:VerifyEntry(fields)
+						if not isEntryValid then  -- Skip pseudo-groups... Another artifact that has to be worked around, I guess
+							self:Print(format(L["Verification failed for entry: %s"], item))
+							numErrors = numErrors + 1
+						end
+						
+					end
+				end
+			end
+			
+			if numErrors == 0 then self:Print(L["Verification complete! Everything appears to be in order..."])
+			else self:Print(format(L["Verfication failed with %d errors!"], numErrors)) end
+
+end
 
 function R:ChatCommand(input)
 	if strlower(input) == "debug" then
@@ -918,35 +952,7 @@ function R:ChatCommand(input)
 		self.DebugCache:PrintMessages(numMessages)
 	elseif strlower(input) == "verify" then -- Verify the ItemDB	
 		
-		local DBH = self.DatabaseMaintenanceHelper
-		local ItemDB = self.db.profile.groups.items
-		local PetDB = self.db.profile.groups.pets
-		local MountDB = self.db.profile.groups.mounts
-		local UserDB = self.db.profile.groups.user
-		local DB = { ItemDB, PetDB, MountDB }
-
-		self:Print(L["Verifying database..."])
-		
-		local numErrors = 0
-		
-		for category, entry in pairs(DB) do
-			for item, fields in pairs(entry) do
-				
-				if type(fields) == "table" then
-				
-					self:Debug(format(L["Verifying entry: %s ..."], item))
-					local isEntryValid = DBH:VerifyEntry(fields)
-					if not isEntryValid then  -- Skip pseudo-groups... Another artifact that has to be worked around, I guess
-						self:Print(format(L["Verification failed for entry: %s"], item))
-						numErrors = numErrors + 1
-					end
-					
-				end
-			end
-		end
-		
-		if numErrors == 0 then self:Print(L["Verification complete! Everything appears to be in order..."])
-		else self:Print(format(L["Verfication failed with %d errors!"], numErrors)) end
+		self:VerifyItemDB()
 		
 	elseif strlower(input) == "profiling" then
 		if self.db.profile.enableProfiling then
