7f14d7a00d3f9312e034c1cddc13abeb929d58da
Sacred Duckwhale <duckwhale@mail.ru>
2019-08-03 22:03:30 +0200

    DB: Added Mechagonian Sawblades (toy)

--- Locales.lua
+++ Locales.lua
@@ -43,6 +43,7 @@ L["Snowsoft Nibbler"] = true
 L["Spraybot 0D"] = true
 L["The Kleptoboss"] = true
 L["Nameless Octopode"] = true
+L["Mechagonian Sawblades"] = true
 L["Experimental"] = true
 L["Verify item database on login"] = true
 L["Run the verification routine automatically after logging in. It can always be run manually (by typing %s)."] = true
