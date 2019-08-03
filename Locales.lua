ad9324b3b02db8efebc8efc96bbb8efc28ee47e9
Sacred Duckwhale <duckwhale@mail.ru>
2019-08-03 22:40:24 +0200

    DB: Added Zanj'ir Weapon Rack (toy)

--- Locales.lua
+++ Locales.lua
@@ -52,6 +52,7 @@ L["Judgment of Mechagon"] = true
 L["The Rusty Prince"] = true
 L["Flopping Fish"] = true
 L["Memento of the Deeps"] = true
+L["Zanj'ir Weapon Rack"] = true
 L["Experimental"] = true
 L["Verify item database on login"] = true
 L["Run the verification routine automatically after logging in. It can always be run manually (by typing %s)."] = true
