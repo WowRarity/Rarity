3be7a31a916379e2a04568bbdebcbf43f4ce666a
SacredDuckwhale <duckwhale@mail.ru>
2020-07-22 13:02:32 +0200

    Format: Auto-formatted the DB maintenance helper utility (for consistency)

--- Utils/DatabaseMaintenanceHelper.lua
+++ Utils/DatabaseMaintenanceHelper.lua
@@ -1,34 +1,31 @@
 local addonName, addon = ...
-if not addon then return end
-
+if not addon then
+	return
+end
 
 -- Upvalues
 local pairs = pairs
 local tostring = tostring
 local assert = assert
 
-
 -- Locals
 -- Format: fieldName = isRequiredField (optional if set to FALSE)
 local DBH = {
-
 	itemTypes = {
-
 		-- Specific fields for each item type
 		PET = {
 			spellId = true,
-			creatureId = true,
+			creatureId = true
 		},
 		MOUNT = {
-			spellId = true,
+			spellId = true
 		},
-		ITEM = { -- TODO: Internally, the item type TOY doesn't exist...
-			isToy = false,
+		ITEM = {
+			-- TODO: Internally, the item type TOY doesn't exist...
+			isToy = false
 		},
-
 		-- Fields that are valid for all types of items
 		ANY = {
-
 			-- index = true, -- always defaults to the displayed name, for now
 
 			-- Required ItemDB fields
@@ -38,7 +35,7 @@ local DBH = {
 			method = true,
 			chance = true,
 			itemId = true,
-		--	arbitraryRequiredTestField = true,
+			--	arbitraryRequiredTestField = true,
 
 			-- Optional ItemDB fields
 			questId = false,
@@ -52,7 +49,6 @@ local DBH = {
 			lockoutDetails = false,
 			instanceDifficulties = false,
 			unique = false,
-
 			-- Populated fields (SavedVariables)
 			attempts = false,
 			lastAttempts = false,
@@ -94,31 +90,24 @@ local DBH = {
 			disableForClass = false,
 			holidayReminder = false,
 			achievementId = false,
-			showTooltipCondition = false,
+			showTooltipCondition = false
 			-- TODO: These fields seem to be all over the place. They should adhere to the structure defined above for each item type... Sigh.
-
-		},
-
-	},
-
-
+		}
+	}
 }
 
 function DBH:VerifyEntry(entry)
-
 	-- print("Verifying entry for item: " .. tostring(entry and entry.name))
 
 	local itemType = entry.type
 	if self.itemTypes[itemType] == nil then
-		print(tostring(itemType) .. " is not a valid item type" )
+		print(tostring(itemType) .. " is not a valid item type")
 		return false
 	end
 
 	-- The most basic check: Make sure all required fields are set
 	for type, fields in pairs(self.itemTypes) do
-
 		for key, isRequiredField in pairs(fields) do -- Check if required fields are actually set
-
 			if isRequiredField and (type == itemType or type == "ANY") then -- This field is required for the given item type
 				if not entry[key] then -- ..	. but it isn't set
 					print(tostring(key) .. " is a required field and must be set")
@@ -127,18 +116,15 @@ function DBH:VerifyEntry(entry)
 				end
 			end
 		end
-
 	end
 
 	-- Additional check: Allow only valid fields (result: Alert if something was entered incorrectly while updating the DB...)
 	for key, value in pairs(entry) do
-
 		if (self.itemTypes.ANY[key] == nil) and (self.itemTypes[itemType][key] == nil) then
 			print(tostring(key) .. " is an invalid field and cannot be set")
 			return false
 		end
 		-- TODO: Type checking, validation etc. here (if ever implemented)
-
 	end
 
 	-- equalOdds and groupSize usually go together since everything is Personal Loot nowadays
@@ -153,28 +139,21 @@ function DBH:VerifyEntry(entry)
 	end
 
 	return true
-
 end
 
-
 function DBH:GetExportString(entry)
-
 	local isValidEntry = self:VerifyEntry(entry)
 	assert(isValidEntry, "Cannot export an invalid entry")
 
-	local exportString = "[\"" .. entry.name .. "\"] = {/n"
+	local exportString = '["' .. entry.name .. '"] = {/n'
 
 	for key, value in pairs(entry) do -- Assemble the string
-
-		exportString = exportString .. key .. " = ".. value .. ","
-
+		exportString = exportString .. key .. " = " .. value .. ","
 	end
 
 	exportString = exportString .. "},\n"
 
 	return exportString
-
 end
-
 
 Rarity.Utils.DatabaseMaintenanceHelper = DBH
