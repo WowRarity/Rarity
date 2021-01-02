3905b4c405a3a255dab5e20de7331237ffa55767
SacredDuckwhale <duckwhale@mail.ru>
2021-01-02 01:01:54 +0100

    Core: Fixed covenant restriction check for un-aligned characters
    'twas an oversight. I can't test this myself since I don't have an
    eligible character :/

--- Core.lua
+++ Core.lua
@@ -657,6 +657,12 @@ function R:IsAttemptAllowed(item)
 	if item.requiresCovenant and item.requiredCovenantID and activeCovenantID ~= item.requiredCovenantID  then
 		local activeCovenantData = C_Covenants.GetCovenantData(activeCovenantID)
 		local requiredCovenantData = C_Covenants.GetCovenantData(item.requiredCovenantID)
+
+		if not activeCovenantData then
+			Rarity:Debug(format("Attempts for item %s are disallowed (Covenant %d/%s is required, but none is currently active)", item.name, item.requiredCovenantID, requiredCovenantData.name))
+			return false
+		end
+
 		Rarity:Debug(format("Attempts for item %s are disallowed (Covenant %d/%s is required, but active covenant is %d/%s)", item.name, item.requiredCovenantID, requiredCovenantData.name, activeCovenantID, activeCovenantData.name))
 		return false
 	end
