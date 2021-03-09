cfa4219baf7c2239862a10e548c1836d60753b98
tommygodejord <tommy.godejord@gmail.com>
2021-03-09 18:58:45 +0100

    DB: #276 Added Dirty Glinting Object as a node

--- DB/Nodes.lua
+++ DB/Nodes.lua
@@ -242,5 +242,6 @@ R.opennodes = {
 	[L["Secret Treasure"]] = true,
 	[L["Forgotten Chest"]] = true,
 	[L["Cache of Eyes"]] = true,
-	[L["Forgotten Chest"]] = true
+	[L["Forgotten Chest"]] = true,
+	[L["Dirty Glinting Object"]] = true
 }
