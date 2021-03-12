66e68b1a7402ea1f7a1bdedc476efd96d5f582ff
SacredDuckwhale <duckwhale@mail.ru>
2021-03-12 18:26:09 +0100

    DB: Re-added Big Love Rocket to the holiday event mounts
    It went AWOL due to a copy/paste error in the latest refactoring pass :/

--- DB/Mounts/HolidayEvents.lua
+++ DB/Mounts/HolidayEvents.lua
@@ -4,6 +4,24 @@ local L = LibStub("AceLocale-3.0"):GetLocale("Rarity")
 local CONSTANTS = addonTable.constants
 
 local holidayEventMounts = {
+	["Big Love Rocket"] = {
+		cat = CONSTANTS.ITEM_CATEGORIES.HOLIDAY,
+		type = CONSTANTS.ITEM_TYPES.MOUNT,
+		method = CONSTANTS.DETECTION_METHODS.USE,
+		name = L["Big Love Rocket"],
+		spellId = 71342,
+		itemId = 50250,
+		items = {54537},
+		chance = 3333,
+		holidayTexture = CONSTANTS.HOLIDAY_TEXTURES.LOVE_IS_IN_THE_AIR,
+		groupSize = 5,
+		equalOdds = true,
+		sourceText = L[
+			"Can be contained in Heart-Shaped Box, rewarded for defeating the World Event Dungeon during Love is in the Air."
+		],
+		lockDungeonId = 288,
+		coords = {{m = 310, x = 40.8, y = 52.6, i = true}}
+	},
 	["Minion of Grumpus"] = {
 		cat = CONSTANTS.ITEM_CATEGORIES.HOLIDAY,
 		type = CONSTANTS.ITEM_TYPES.MOUNT,
