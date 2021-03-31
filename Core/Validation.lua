54b0794f98112b91722b07ec00845cf44feed960
Duckwhale <duckwhale@mail.ru>
2021-03-31 06:50:42 +0200

    Validation: Fixed references to the Rarity module

--- Core/Validation.lua
+++ Core/Validation.lua
@@ -5,23 +5,23 @@ local Validation = {}
 local L = addonTable.L
 
 function Validation:ValidateItemDB()
-	local ItemDB = self.db.profile.groups.items
-	local PetDB = self.db.profile.groups.pets
-	local MountDB = self.db.profile.groups.mounts
-	local UserDB = self.db.profile.groups.user
+	local ItemDB = Rarity.db.profile.groups.items
+	local PetDB = Rarity.db.profile.groups.pets
+	local MountDB = Rarity.db.profile.groups.mounts
+	local UserDB = Rarity.db.profile.groups.user
 	local DB = {ItemDB, PetDB, MountDB}
 
-	self:Print(L["Verifying item database..."])
+	Rarity:Print(L["Verifying item database..."])
 
 	local numErrors = 0
 
 	for category, entry in pairs(DB) do
 		for item, fields in pairs(entry) do
 			if type(fields) == "table" then
-				self:Debug(format(L["Verifying entry: %s ..."], item))
+				Rarity:Debug(format(L["Verifying entry: %s ..."], item))
 				local isEntryValid = Rarity.Validation:VerifyEntry(fields)
 				if not isEntryValid then -- Skip pseudo-groups... Another artifact that has to be worked around, I guess
-					self:Print(format(L["Verification failed for entry: %s"], item))
+					Rarity:Print(format(L["Verification failed for entry: %s"], item))
 					numErrors = numErrors + 1
 				end
 			end
@@ -29,9 +29,9 @@ function Validation:ValidateItemDB()
 	end
 
 	if numErrors == 0 then
-		self:Print(L["Verification complete! Everything appears to be in order..."])
+		Rarity:Print(L["Verification complete! Everything appears to be in order..."])
 	else
-		self:Print(format(L["Verfication failed with %d errors!"], numErrors))
+		Rarity:Print(format(L["Verfication failed with %d errors!"], numErrors))
 	end
 end
 
