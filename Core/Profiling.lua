db4719edfbd5c5d247def9886c7c3b194001ccee
RDW <duckwhale@mail.ru>
2021-07-17 05:59:51 +0200

    Core: Implemented a more streamlined Profiling API
    Relying on debugprofilestart directly is a bit risky, and the existing Profiling code only supports two groups. In order to debug the performance issues more easily, a more powerful profiling API should prove helpful.
    
    We can later remove the legacy profiling code, or gradually transition to the streamlined API. This one is based on the Chromium/Firefox console APIs and can be nested indefinitely as long as unique labels are used.
    
    Cleanup: Removed isTimerRunning property from Profling API
    
    It's unused and should never have been added in the first place.

--- Core/Profiling.lua
+++ Core/Profiling.lua
@@ -1,8 +1,71 @@
 local _, addonTable = ...
 
-local Profiling = {}
+local Profiling = {
+	DEFAULT_TIMER_LABEL = "Default",
+	activeTimers = {},
+	accumulatedTimes = {}
+}
 
--- What's with this file? Looks like the profiling code should go there, but right now it is all over the place
+function Profiling:StartTimer(label)
+	if not self:IsProfilingEnabled() then
+		return
+	end
+
+	label = label or self.DEFAULT_TIMER_LABEL
+
+	if self.activeTimers[label] then
+		Rarity:Debug("Failed to start timer %s (already running)", label) -- Should be a NOTICE or similar
+		return
+	end
+
+	local startTime = self:GetCurrentTimeInMilliseconds()
+	self.activeTimers[label] = startTime
+end
+
+local GetTimePreciseSec = _G.GetTimePreciseSec
+local MS_PER_SECOND = 1000
+
+function Profiling:GetCurrentTimeInMilliseconds()
+	return GetTimePreciseSec() * MS_PER_SECOND
+	-- Relying on debugprofilestop() is risky, as it could be reset globally via debugprofilestart()
+	-- return debugprofilestop()
+end
+
+function Profiling:EndTimer(label)
+	if not self:IsProfilingEnabled() then
+		return
+	end
+
+	label = label or self.DEFAULT_TIMER_LABEL
+
+	local startTime = self.activeTimers[label]
+
+	if not startTime then
+		Rarity:Debug("Failed to end timer %s (no such timer is active)", label)
+		return
+	end
+
+	local endTime = self:GetCurrentTimeInMilliseconds()
+	local elapsedTimeInMilliseconds = endTime - startTime
+
+	self.activeTimers[label] = nil
+
+	Rarity:ProfileDebug(format("%s: %.2f ms", label, elapsedTimeInMilliseconds))
+end
+
+function Profiling:GetTimer(label)
+	if not self:IsProfilingEnabled() then
+		return
+	end
+
+	label = label or self.DEFAULT_TIMER_LABEL
+
+	return self.activeTimers[label]
+end
+
+function Profiling:IsProfilingEnabled()
+	return Rarity.db.profile.enableProfiling
+end
 
 Rarity.Profiling = Profiling
 return Profiling
