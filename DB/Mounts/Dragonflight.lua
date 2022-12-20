6a7c209c5060458a18b032356f28e3cbe947c29f
Tommy Godejord <tommy.godejord@gmail.com>
2022-12-20 20:00:40 +0100

    DB: Added Reins of the Liberated Slyvern

--- DB/Mounts/Dragonflight.lua
+++ DB/Mounts/Dragonflight.lua
@@ -3,6 +3,19 @@ local addonName, addonTable = ...
 local L = LibStub("AceLocale-3.0"):GetLocale("Rarity")
 local CONSTANTS = addonTable.constants
 
-local dragonflightMounts = {}
+local dragonflightMounts = {
+	-- 10.0 mounts
+	["Reins of the Liberated Slyvern"] = {
+		cat = CONSTANTS.ITEM_CATEGORIES.DRAGONFLIGHT,
+		type = CONSTANTS.ITEM_TYPES.MOUNT,
+		method = CONSTANTS.DETECTION_METHODS.NPC,
+		name = L["Reins of the Liberated Slyvern"],
+		itemId = 201440,
+		spellId = 359622,
+		npcs = { 195353 },
+		chance = 50,
+		coords = { { x = 27.0, y = 48.4, n = L["Breezebiter"], m = CONSTANTS.UIMAPIDS.THE_AZURE_SPAN } },
+	},
+}
 
 Rarity.ItemDB.MergeItems(Rarity.ItemDB.mounts, dragonflightMounts)
