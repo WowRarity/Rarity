7122e311601cc9fc664546f9ba5a2ee7fdd44c1e
RDW <duckwhale@mail.ru>
2023-08-03 04:17:21 +0200

    Refactor: Moved all Base64 logic to the Serialization module

--- Modules/Options/Options.lua
+++ Modules/Options/Options.lua
@@ -249,57 +249,6 @@ local function allitems()
 	return t
 end
 
-local b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-
-local function enc64(data)
-	return (
-		(data:gsub(".", function(x)
-			local r, byte = "", x:byte()
-			for i = 8, 1, -1 do
-				r = r .. (byte % 2 ^ i - byte % 2 ^ (i - 1) > 0 and "1" or "0")
-			end
-			return r
-		end) .. "0000"):gsub("%d%d%d?%d?%d?%d?", function(x)
-			if #x < 6 then
-				return ""
-			end
-			local c = 0
-			for i = 1, 6 do
-				c = c + (x:sub(i, i) == "1" and 2 ^ (6 - i) or 0)
-			end
-			return b:sub(c + 1, c + 1)
-		end) .. ({ "", "==", "=" })[#data % 3 + 1]
-	)
-end
-
-local function dec64(data)
-	if not data then
-		return nil
-	end
-	data = string.gsub(data, "[^" .. b .. "=]", "")
-	return (
-		data:gsub(".", function(x)
-			if x == "=" then
-				return ""
-			end
-			local r, f = "", (b:find(x) - 1)
-			for i = 6, 1, -1 do
-				r = r .. (f % 2 ^ i - f % 2 ^ (i - 1) > 0 and "1" or "0")
-			end
-			return r
-		end):gsub("%d%d%d?%d?%d?%d?%d?%d?", function(x)
-			if #x ~= 8 then
-				return ""
-			end
-			local c = 0
-			for i = 1, 8 do
-				c = c + (x:sub(i, i) == "1" and 2 ^ (8 - i) or 0)
-			end
-			return string.char(c)
-		end)
-	)
-end
-
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- METHODS
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -1245,7 +1194,7 @@ function R:PrepareOptions()
 							local s
 
 							-- Validate the import (this also can disable the Import button, so we only do validation here)
-							local enc = dec64(self.db.profile.lastImportString)
+							local enc = Rarity.Serialization:DecodeBase64(self.db.profile.lastImportString)
 							if not enc then
 								self.db.profile.importIsError = true
 								s = colorize(L["The selected Rarity Item Pack string is invalid."], red)
@@ -1496,7 +1445,7 @@ function R:PrepareOptions()
 								alert(L["Error compressing item pack"])
 								return
 							end
-							local enc = enc64(c)
+							local enc = Rarity.Serialization:EncodeBase64(c)
 							if not enc then
 								alert(L["Error encoding item pack"])
 								return
