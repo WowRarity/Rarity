89d1913c05bfb31c14b99636c57a7f8ce31bc8d5
RDW <duckwhale@mail.ru>
2023-08-04 12:15:40 +0200

    DB: Added Echo of the Depths (pet)

--- Locales.lua
+++ Locales.lua
@@ -2,6 +2,7 @@ local L
 L = LibStub("AceLocale-3.0"):NewLocale("Rarity", "enUS", true)
 
 -- L["AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"] = true
+L["Echo of the Depths"] = true
 L["Echo of the Heights"] = true
 L["Shaggy"] = true
 L["Doomrubble"] = true
