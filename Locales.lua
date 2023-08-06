046412690dcc852836d33ad27f3b7f188ae21eb3
cyriun <orangepeel2001@gmail.com>
2023-08-06 17:17:38 +1000

    DB: Added Everlasting Horn of Lavaswimming (toy)

--- Locales.lua
+++ Locales.lua
@@ -1988,6 +1988,8 @@ L["Gooey Snailemental"] = true
 L["Dropped from the final bosses of Froststone Vault Primal Storm."] = true
 L["Combine 50 Leftover Elemental Slime to create the Gooey Snailemental."] = true
 L["Froststone Vault Primal Storm"] = true
+L["Everlasting Horn of Lavaswimming"] = true
+L["This toy is obtained at the end of the Siege on Dragonbane Keep event."] = true
 
 --[[
 					The rest of this file is auto-generated using the WoWAce localization application.
