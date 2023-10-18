09caa7085bee6f0f98b41f6d1a4d97e2d1fcfbbe
RDW <duckwhale@mail.ru>
2023-10-19 01:58:00 +0200

    DB: Added a new entry for Arfus (pet)

--- DB/Pets/HolidayEvents.lua
+++ DB/Pets/HolidayEvents.lua
@@ -263,6 +263,23 @@ local holidayEventPets = {
 		coords = { { m = CONSTANTS.UIMAPIDS.DARKMOON_ISLAND, x = 75.6, y = 86.8 } },
 		holidayTexture = CONSTANTS.HOLIDAY_TEXTURES.DARKMOON_FAIRE,
 	},
+	["Arfus"] = {
+		cat = CONSTANTS.ITEM_CATEGORIES.HOLIDAY,
+		type = CONSTANTS.ITEM_TYPES.PET,
+		method = CONSTANTS.DETECTION_METHODS.USE,
+		name = L["Arfus"],
+		spellId = 406225,
+		itemId = 211271,
+		items = { 209024 },
+		chance = 100, -- Blind guess
+		groupSize = 5,
+		equalOdds = true,
+		creatureId = 203463,
+		lockDungeonId = 285,
+		holidayTexture = CONSTANTS.HOLIDAY_TEXTURES.HALLOWS_END,
+		coords = { { m = 435, i = true } },
+		sourceText = L["Can be contained in Loot-Filled Pumpkin, rewarded for defeating the World Event Dungeon during Hallow's End."],
+	},
 }
 
 Rarity.ItemDB.MergeItems(Rarity.ItemDB.pets, holidayEventPets)
