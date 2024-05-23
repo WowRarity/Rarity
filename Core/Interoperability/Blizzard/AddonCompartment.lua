cb78d34afe3922356e1a92347dd7890ad5909d0f
RDW <duckwhale@mail.ru>
2024-05-24 00:20:45 +0200

    Core: Wired up the addon compartment hooks to the options
    It's unclear whether the UX could be improved by providing more options here than just "show the UI". For now, it'll just be identical in behavior to the /rarity slash command since I've no idea how people are using the new addon compartment feature.

--- Core/Interoperability/Blizzard/AddonCompartment.lua
+++ Core/Interoperability/Blizzard/AddonCompartment.lua
@@ -1,15 +1,25 @@
+local L = LibStub("AceLocale-3.0"):GetLocale("Rarity")
+
 local AddonCompartment = {}
 
 function AddonCompartment.OnClick()
-	print("[Placeholder] Rarity.AddonCompartment.OnClick triggered")
+	-- Should behave the same as the default /rarity slash command (copy/pasted for now) - improve UX later?
+	LoadAddOn("Rarity_Options")
+	if Rarity.optionsFrame then
+		-- Thanks, Blizzard (https://www.wowinterface.com/forums/showthread.php?t=54599)
+		InterfaceOptionsFrame_OpenToCategory(Rarity.optionsFrame)
+		InterfaceOptionsFrame_OpenToCategory(Rarity.optionsFrame)
+	else
+		Rarity:Print(L["The Rarity Options module has been disabled. Log out and enable it from your add-ons menu."])
+	end
 end
 
 function AddonCompartment.OnEnter()
-	print("[Placeholder] Rarity.AddonCompartment.OnEnter triggered")
+	-- Might want to display a tooltip here if the behavior differs from the default /rarity slash command
 end
 
 function AddonCompartment.OnLeave()
-	print("[Placeholder] Rarity.AddonCompartment.OnLeave triggered")
+	-- Might want to display a tooltip here if the behavior differs from the default /rarity slash command
 end
 
 -- No way around polluting the global namespace for these, AFAICT
