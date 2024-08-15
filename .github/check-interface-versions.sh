6f60a2a7715363bd24c131b372adc674c6779854
RDW <duckwhale@mail.ru>
2024-08-15 20:25:56 +0200

    CI: Enabled the TOC version check to process lists
    Piling more hacks onto this sketchy script, it now extracts the highest (latest) version number if there are multiple listed.
    
    I don't expect the approach to be any less brittle, but at least it should again function as an alert in case of outdated TOC versions.

--- .github/check-interface-versions.sh
+++ .github/check-interface-versions.sh
@@ -2,7 +2,7 @@ local_version=$(cat Rarity.toc | grep -oP '## Interface: \K\d+')
 options_version=$(cat Modules/Options/Rarity_Options.toc | grep -oP '## Interface: \K\d+')
 
 # Since there's no "official" way to get the latest version, just use a popular/frequently updated addon ...
-remote_version=$(curl https://raw.githubusercontent.com/BigWigsMods/BigWigs/master/BigWigs.toc --silent | grep -oP '## Interface: \K\d+')
+remote_version=$(curl https://raw.githubusercontent.com/BigWigsMods/BigWigs/master/BigWigs.toc --silent | grep -oP '## Interface:.*' | grep -oP '\d+' | sort -nr | head -n 1)
 
 if [ "$local_version" != "$remote_version" ]; then
     echo "Local interface version ($local_version) is not up to date with remote version ($remote_version)"
