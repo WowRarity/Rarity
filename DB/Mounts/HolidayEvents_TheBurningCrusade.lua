6ba2fda3ca8e0b90baa38fed5e173b428598609b
aSnejbjerg <42168095+aSnejbjerg@users.noreply.github.com>
2025-02-06 19:40:48 +0100

    Moved Holiday items to expansion specific files
    Moved Holiday items into expansion specific files so it's easier to manage between game versions.

---
+++ DB/Mounts/HolidayEvents_TheBurningCrusade.lua
@@ -0,0 +1,58 @@
+local addonName, addonTable = ...
+
+local L = LibStub("AceLocale-3.0"):GetLocale("Rarity")
+local CONSTANTS = addonTable.constants
+
+local holidayEventMountsTBC = {
+	["Great Brewfest Kodo"] = {
+		cat = CONSTANTS.ITEM_CATEGORIES.HOLIDAY,
+		type = CONSTANTS.ITEM_TYPES.MOUNT,
+		method = CONSTANTS.DETECTION_METHODS.USE,
+		name = L["Great Brewfest Kodo"],
+		spellId = 49379,
+		itemId = 37828,
+		items = { 54535, 117393 },
+		chance = 25,
+		groupSize = 5,
+		equalOdds = true,
+		sourceText = L["Can be contained in Keg-Shaped Treasure Chest, rewarded for defeating the World Event Dungeon during Brewfest."],
+		lockDungeonId = 287,
+		holidayTexture = CONSTANTS.HOLIDAY_TEXTURES.BREWFEST,
+		coords = { { m = 243, x = 46.6, y = 60, i = true } },
+	},
+	["Swift Brewfest Ram"] = {
+		cat = CONSTANTS.ITEM_CATEGORIES.HOLIDAY,
+		type = CONSTANTS.ITEM_TYPES.MOUNT,
+		method = CONSTANTS.DETECTION_METHODS.USE,
+		name = L["Swift Brewfest Ram"],
+		spellId = 43900,
+		itemId = 33977,
+		items = { 54535, 117393 },
+		chance = 25,
+		groupSize = 5,
+		equalOdds = true,
+		sourceText = L["Can be contained in Keg-Shaped Treasure Chest, rewarded for defeating the World Event Dungeon during Brewfest."],
+		lockDungeonId = 287,
+		holidayTexture = CONSTANTS.HOLIDAY_TEXTURES.BREWFEST,
+		coords = { { m = 242, x = 46.6, y = 60, i = true } },
+	},
+	["The Horseman's Reins"] = {
+		cat = CONSTANTS.ITEM_CATEGORIES.HOLIDAY,
+		type = CONSTANTS.ITEM_TYPES.MOUNT,
+		method = CONSTANTS.DETECTION_METHODS.USE,
+		name = L["The Horseman's Reins"],
+		spellId = 48025,
+		itemId = 37012,
+		items = { 117392, 209024 },
+		chance = 200,
+		groupSize = 5,
+		equalOdds = true,
+		sourceText = L["Can be contained in Loot-Filled Pumpkin, rewarded for defeating the World Event Dungeon during Hallow's End."],
+		lockDungeonId = 285,
+		holidayTexture = CONSTANTS.HOLIDAY_TEXTURES.HALLOWS_END,
+		coords = { { m = 435, i = true } },
+	},
+	
+}
+
+Rarity.ItemDB.MergeItems(Rarity.ItemDB.mounts, holidayEventMountsTBC)
---
+++ DB/Toys/HolidayEvents_TheBurningCrusade.lua
@@ -0,0 +1,23 @@
+local addonName, addonTable = ...
+
+local L = LibStub("AceLocale-3.0"):GetLocale("Rarity")
+local CONSTANTS = addonTable.constants
+
+local holidayEventToysTBC = {
+	["Crashin' Thrashin' Racer Controller"] = {
+		cat = CONSTANTS.ITEM_CATEGORIES.HOLIDAY,
+		type = CONSTANTS.ITEM_TYPES.ITEM,
+		isToy = true,
+		method = CONSTANTS.DETECTION_METHODS.USE,
+		name = L["Crashin' Thrashin' Racer Controller"],
+		itemId = 37710,
+		items = { 116762 },
+		chance = 50,
+		holidayTexture = CONSTANTS.HOLIDAY_TEXTURES.WINTERS_VEIL,
+		questId = { 6983, 7043 },
+		coords = { { m = CONSTANTS.UIMAPIDS.HILLSBRAD_FOOTHILLS, x = 43.6, y = 39.6 } },
+	},
+	
+}
+
+Rarity.ItemDB.MergeItems(Rarity.ItemDB.toys, holidayEventToysTBC)
