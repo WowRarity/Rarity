3b4af84f9c756c26eadd3fbc13e2347e2729ac90
RDW <rdw-software@proton.me>
2025-02-25 23:54:21 +0100

    CI: Enabled checking multiple CDN versions at once
    This script isn't going to win any prizes for elegance, but let's not overdo it.
    
    Pretty sure it needs to be rewritten anyway if more complex use cases have to be supported.

--- .github/check-interface-versions.sh
+++ .github/check-interface-versions.sh
@@ -1,4 +1,6 @@
 set -eu
 
-curl --silent --show-error https://us.version.battle.net/v2/products/wow/versions > cdn-response.txt
-evo Tools/check-cdn-version.lua cdn-response.txt
+curl --silent --show-error https://us.version.battle.net/v2/products/wow/versions > retail-mainline-versions.txt
+curl --silent --show-error https://us.version.battle.net/v2/products/wow_classic/versions > cata-classic-versions.txt
+curl --silent --show-error https://us.version.battle.net/v2/products/wow_classic_era/versions > classic-era-versions.txt
+evo Tools/check-cdn-version.lua retail-mainline-versions.txt cata-classic-versions.txt classic-era-versions.txt
--- Tools/check-cdn-version.lua
+++ Tools/check-cdn-version.lua
@@ -4,24 +4,35 @@ local CDN = require("Tests.TOC.CDN")
 local transform = require("transform")
 local bold = transform.bold
 
-local cdnResponseText = C_FileSystem.ReadFile(arg[1])
-C_FileSystem.Delete(arg[1])
-local coreAddonVersion = arg[2]
-local optionsAddonVersion = arg[3]
+local cdnResponseTexts = {
+	C_FileSystem.ReadFile(arg[1]),
+	C_FileSystem.ReadFile(arg[2]),
+	C_FileSystem.ReadFile(arg[3]),
+}
 
-printf("Parsing CDN response:\n%s", transform.cyan(cdnResponseText))
-local response = CDN:ParseResponseText(cdnResponseText)
-printf(transform.yellow("Sequence number: %d"), response.sequenceNumber)
-printf(transform.yellow("Region keys: %s"), dump(table.keys(response.productInfoByRegion), { silent = true }))
+assert(C_FileSystem.Delete(arg[1]))
+assert(C_FileSystem.Delete(arg[2]))
+assert(C_FileSystem.Delete(arg[3]))
 
-print()
+local cdnResponses = {}
 
-for regionKey, productInfo in pairs(response.productInfoByRegion) do
-	if regionKey == "us" then
-		for key, value in pairs(productInfo) do
-			printf(bold("%s") .. ": %s", key, value)
+for index, cdnResponseText in ipairs(cdnResponseTexts) do
+	printf("Parsing CDN response:\n%s", transform.cyan(cdnResponseText))
+	local response = CDN:ParseResponseText(cdnResponseText)
+	printf(transform.yellow("Sequence number: %d"), response.sequenceNumber)
+	printf(transform.yellow("Region keys: %s"), dump(table.keys(response.productInfoByRegion), { silent = true }))
+
+	print()
+
+	for regionKey, productInfo in pairs(response.productInfoByRegion) do
+		if regionKey == "us" then
+			for key, value in pairs(productInfo) do
+				printf(bold("%s") .. ": %s", key, value)
+			end
 		end
 	end
+
+	table.insert(cdnResponses, response)
 end
 
 print()
@@ -35,27 +46,30 @@ for moduleName, tocFilePath in pairs(tocFiles) do
 	local tocFileContents = C_FileSystem.ReadFile(tocFilePath)
 	printf("Processing TOC file: %s -> %s", bold(moduleName), bold(tocFilePath))
 	local toc = BlizzardTOC:DecodeFileContents(tocFileContents)
+	assert(#toc["Interface"] == #cdnResponses, "Expected one TOC version per supported product line")
 
-	local tocInterfaceVersion = toc["Interface"]
-	printf(bold("Detected interface version: %d"), tocInterfaceVersion)
+	for index, response in ipairs(cdnResponses) do
+		local tocInterfaceVersion = toc["Interface"][index]
+		printf(bold("Detected interface version: %s"), tocInterfaceVersion)
 
-	-- Assumes the US CDN is authoritative (should be the earliest to update?)
-	local usVersionName = response.productInfoByRegion.us.VersionsName
-	local latestInterfaceVersion = CDN:VersionNameToInterfaceVersion(usVersionName)
+		-- Assumes the US CDN is authoritative (should be the earliest to update?)
+		local usVersionName = response.productInfoByRegion.us.VersionsName
+		local latestInterfaceVersion = assert(CDN:VersionNameToInterfaceVersion(usVersionName))
 
-	if tocInterfaceVersion ~= latestInterfaceVersion then
-		local errorMessage = format(
-			"✗ Local TOC interface version %d does NOT match Blizzard CDN version %d",
-			tocInterfaceVersion,
-			latestInterfaceVersion
-		)
-		error(transform.red(errorMessage))
-	else
-		printf(
-			transform.green("✓ Local TOC interface version %d matches Blizzard CDN version %d"),
-			tocInterfaceVersion,
-			latestInterfaceVersion
-		)
+		if tocInterfaceVersion ~= latestInterfaceVersion then
+			local errorMessage = format(
+				"✗ Local TOC interface version %d does NOT match Blizzard CDN version %d",
+				tocInterfaceVersion,
+				latestInterfaceVersion
+			)
+			error(transform.red(errorMessage))
+		else
+			printf(
+				transform.green("✓ Local TOC interface version %d matches Blizzard CDN version %d"),
+				tocInterfaceVersion,
+				latestInterfaceVersion
+			)
+		end
+		print()
 	end
-	print()
 end
