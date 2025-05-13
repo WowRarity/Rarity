2c8466303a9c878c2d3d3a606c556dc5b98324d2
RDW <rdw-software@proton.me>
2025-05-13 04:41:56 +0200

    Refactor: Move GetNPCIDFromGUID to the GUID module

--- Core.lua
+++ Core.lua
@@ -691,17 +691,6 @@ function R:UpdateInterestingThings()
 	end
 end
 
-function R:GetNPCIDFromGUID(guid)
-	if guid then
-		local unit_type, _, _, _, _, mob_id = strsplit("-", guid)
-		if unit_type == "Pet" or unit_type == "Player" then
-			return 0
-		end
-		return (guid and mob_id and tonumber(mob_id)) or 0
-	end
-	return 0
-end
-
 function R:IsAttemptAllowed(item)
 	-- No item supplied; assume it's okay
 	if item == nil then
--- Core/GUID.lua
+++ Core/GUID.lua
@@ -2,4 +2,15 @@ local _, addonTable = ...
 
 local GUID = {}
 
+function Rarity:GetNPCIDFromGUID(guid)
+	if guid then
+		local unit_type, _, _, _, _, mob_id = strsplit("-", guid)
+		if unit_type == "Pet" or unit_type == "Player" then
+			return 0
+		end
+		return (guid and mob_id and tonumber(mob_id)) or 0
+	end
+	return 0
+end
+
 return GUID
