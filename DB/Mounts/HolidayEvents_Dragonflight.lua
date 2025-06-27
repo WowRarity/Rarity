aec5ced9d1e7863a270ecc124bea192db5faa377
Ellezanor <217450120+Ellezanor@users.noreply.github.com>
2025-06-27 12:17:32 +0100

    Greedy Emissary mounts

--- DB/Mounts/HolidayEvents_Dragonflight.lua
+++ DB/Mounts/HolidayEvents_Dragonflight.lua
@@ -23,6 +23,27 @@ local holidayEventMountsDragonflight = {
 			{ m = CONSTANTS.UIMAPIDS.ELWYNN_FOREST },
 		},
 	},
+	["Tyrael's Charger"] = {
+		cat = CONSTANTS.ITEM_CATEGORIES.HOLIDAY,
+		type = CONSTANTS.ITEM_TYPES.MOUNT,
+		method = CONSTANTS.DETECTION_METHODS.NPC,
+		name = L["Tyrael's Charger"],
+		spellId = 107203,
+		itemId = 76755,
+		npcs = { 205490 },
+		chance = 100, 
+		},
+	},
+	["Inarius' Charger"] = {
+		cat = CONSTANTS.ITEM_CATEGORIES.HOLIDAY,
+		type = CONSTANTS.ITEM_TYPES.MOUNT,
+		method = CONSTANTS.DETECTION_METHODS.NPC,
+		name = L["Inarius' Charger"],
+		spellId = 1241429,
+		itemId = 246264,
+		npcs = { 205490 },
+		chance = 100, 
+	},
 }
 
 Rarity.ItemDB.MergeItems(Rarity.ItemDB.mounts, holidayEventMountsDragonflight)
