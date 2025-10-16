c3b008321a618159a4423f92664e53901ee4eee6
RDW <rdw-software@proton.me>
2025-10-16 23:01:11 +0200

    DB: Add Love Witch's Sweeper

--- Locales.lua
+++ Locales.lua
@@ -2,6 +2,7 @@ local L
 L = LibStub("AceLocale-3.0"):NewLocale("Rarity", "enUS", true)
 
 -- L["AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"] = true
+L["Love Witch's Sweeper"] = true
 L["Show Spark"] = true
 L["Xanthous Siphonmite"] = true
 L["Vitriolic Inchshifter"] = true
