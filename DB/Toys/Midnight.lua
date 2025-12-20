04c6212fccd9c6b0d40de8c1e16a1eb5a37a84f1
Ellezanor <217450120+Ellezanor@users.noreply.github.com>
2025-12-20 11:56:58 +0000

    Create Midnight.lua

---
+++ DB/Toys/Midnight.lua
@@ -0,0 +1,11 @@
+local addonName, addonTable = ...
+
+local L = LibStub("AceLocale-3.0"):GetLocale("Rarity")
+local CONSTANTS = addonTable.constants
+
+if LE_EXPANSION_LEVEL_CURRENT < LE_EXPANSION_MIDNIGHT then
+	return {}
+end
+
+local MidnightToysAndItems = {
+	-- 12.0 items and toys
