51292cfeb13fbc1f35d98afb95505b922be5fb7b
RDW <rdw-software@proton.me>
2026-02-11 01:44:46 +0100

    DB: Add Mythic raid mounts from The War Within
    As is tradition, the formerly guaranteed "prestige" rewards turned into 1% rare drops.
    
    Co-Authored-By: Ellezanor <217450120+Ellezanor@users.noreply.github.com>

--- Locales.lua
+++ Locales.lua
@@ -2,6 +2,9 @@ local L
 L = LibStub("AceLocale-3.0"):NewLocale("Rarity", "enUS", true)
 
 -- L["AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"] = true
+L["Unbound Star-Eater"] = true
+L["Keys to the Big G"] = true
+L["Reins of the Ascendant Skyrazor"] = true
 L["Black Whirlwind"] = true
 L["Only available on the first attempt each day."] = true
 L["The Headless Horseman's Ghoulish Charger"] = true
