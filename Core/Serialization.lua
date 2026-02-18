c17057bb5d83c77ea4441870dbf06a9aeb6b5271
RDW <rdw-software@proton.me>
2026-02-18 01:29:32 +0100

    Update Serialization.lua

--- Core/Serialization.lua
+++ Core/Serialization.lua
@@ -83,7 +83,13 @@ function Serialization:CanItemBeExportedImported(item)
 		end
 	end
 
-	if item.method == CONSTANTS.DETECTION_METHODS.NPC or item.method == CONSTANTS.DETECTION_METHODS.BOSS then -- TODO
+	if item.method == CONSTANTS.DETECTION_METHODS.ENCOUNTER then
+		if not item.encounters or #item.encounters <= 0 then
+			return false
+		end
+	end
+
+	if item.method == CONSTANTS.DETECTION_METHODS.NPC or item.method == CONSTANTS.DETECTION_METHODS.BOSS then
 		if (not item.npcs or #item.npcs <= 0) and (not item.statisticId or #item.statisticId <= 0) then
 			return false
 		end
