1daae4884bb7b05c691bd9c9b2005e35f4f1097f
Ellezanor <217450120+Ellezanor@users.noreply.github.com>
2026-03-13 14:27:50 +0000

    DB: Add Cosmic Ritual Stone (toy)
    Co-Authored-By: Ellezanor <217450120+Ellezanor@users.noreply.github.com>

--- DB/SharedConstants.lua
+++ DB/SharedConstants.lua
@@ -163,6 +163,7 @@ C.UIMAPIDS = {
 	MAGISTERS_TERRACE_MIDNIGHT = 2520, -- Not sure which subzone (can adjust later)
 	WINDRUNNER_SPIRE = 2492,
 	BLINDING_VALE = 2500,
+	VOIDSPIRE = 2529,
 }
 
 -- Types of items
