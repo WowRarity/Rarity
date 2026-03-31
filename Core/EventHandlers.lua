e21c27748f9ba34e096d7a2215c928fbb5832ab4
RDW <rdw-software@proton.me>
2026-03-31 04:19:40 +0200

    Core: Remove event handling for COMBAT_LOG_EVENT_UNFILTERED
    There's really no good reason to use this in Classic if encounter IDs (or, even better, kill statistics) are also available. In fact, there's probably no unlootable enemies worth tracking that aren't bosses in the game.  On Retail, the event doesn't exist.
    
    Since Classic support is "best effort" and incomplete, I'll just remove this for now.

--- Core/EventHandlers.lua
+++ Core/EventHandlers.lua
@@ -63,7 +63,6 @@ function EventHandlers:Register()
 	self:RegisterEvent("LOOT_READY", "OnLootReady")
 	self:RegisterEvent("CURRENCY_DISPLAY_UPDATE", "OnCurrencyUpdate")
 	self:RegisterEvent("RESEARCH_ARTIFACT_COMPLETE", "OnResearchArtifactComplete")
-	self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED", "OnCombat") -- Used to detect boss kills that we didn't solo
 	self:RegisterEvent("CURSOR_CHANGED", "OnCursorChanged") -- Fishing detection
 	self:RegisterEvent("UNIT_SPELLCAST_SENT", "OnSpellcastSent") -- Fishing detection
 	self:RegisterEvent("UNIT_SPELLCAST_STOP", "OnSpellcastStopped") -- Fishing detection
@@ -405,58 +404,6 @@ do
 			Rarity:ScanStatistics(event .. " 6")
 		end, 20)
 	end
-end
-
--------------------------------------------------------------------------------------
--- Handle boss kills. You may not ever open a loot window on a boss, so we need to watch the combat log for its death.
--- This event also handles some special cases.
--------------------------------------------------------------------------------------
-function R:OnCombat()
-	self.Profiling:StartTimer("EventHandlers.OnCombat")
-
-	-- Extract event payload (it's no longer being passed by the event iself as of 8.0.1)
-	local timestamp, eventType, hideCaster, srcGuid, srcName, srcFlags, srcRaidFlags, dstGuid, dstName, dstFlags, dstRaidFlags, spellId, spellName, spellSchool, auraType =
-		CombatLogGetCurrentEventInfo()
-
-	if eventType == "UNIT_DIED" then -- A unit died near you
-		local npcid = self:GetNPCIDFromGUID(dstGuid)
-		if Rarity.bosses[npcid] then -- It's a boss we're interested in
-			R:Debug("Detected UNIT_DIED for relevant NPC with ID = " .. tostring(npcid))
-			if
-				bit_band(srcFlags, COMBATLOG_OBJECT_AFFILIATION_MINE)
-				or bit_band(srcFlags, COMBATLOG_OBJECT_AFFILIATION_PARTY)
-				or bit_band(srcFlags, COMBATLOG_OBJECT_AFFILIATION_RAID)
-			then -- You, a party member, or a raid member killed it
-				if not Rarity.guids[dstGuid] then
-					if not UnitAffectingCombat("player") and not UnitIsDead("player") then
-						Rarity:Debug("Ignoring this UNIT_DIED event because the player is alive, but not in combat")
-						self.Profiling:EndTimer("EventHandlers.OnCombat")
-						return
-					end
-
-					-- Increment attempts counter(s). One NPC might drop multiple things we want, so scan for them all.
-					if Rarity.npcs_to_items[npcid] and type(Rarity.npcs_to_items[npcid]) == "table" then
-						for k, v in pairs(Rarity.npcs_to_items[npcid]) do
-							local isBossDrop = (v.method == CONSTANTS.DETECTION_METHODS.BOSS)
-							local hasKillStatistics = type(v.statisticId) ~= "nil"
-							if v.enabled ~= false and isBossDrop and not hasKillStatistics then
-								if self:IsAttemptAllowed(v) then
-									Rarity.guids[dstGuid] = true
-									if v.attempts == nil then
-										v.attempts = 1
-									else
-										v.attempts = v.attempts + 1
-									end
-									self:OutputAttempts(v)
-								end
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-	self.Profiling:EndTimer("EventHandlers.OnCombat")
 end
 
 local worldEventQuests = {
